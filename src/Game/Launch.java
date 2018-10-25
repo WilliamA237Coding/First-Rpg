aceb70cc96d5f31563e711b951e41e636895f8ee
WilliamACoding237 <andersonwilliam236@gmail.com>
2018-10-25 01:23:30 -0400

    Minor setup

--- src/Game/Launch.java
+++ src/Game/Launch.java
@@ -1,4 +1,19 @@
+/**
+ * A simple RPG game
+ * @author William Anderson andersonwilliam236@gmail.com
+ */
+
+
 package Game;
 
 public class Launch {
+
+    /**Game Mode Options**/
+    public static boolean RESPAWN;
+
+    /**Hack Options**/
+    public static boolean GODMODE, UNKILLABLE;
+
+
+
 }
